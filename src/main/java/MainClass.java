1b2d2cf90dfd0b2c8f99f861eb3b23aeabf3bb4f
root <root@instance-1.c.first-project-181415.internal>
2017-12-14 18:50:17 +0000

    Added log4j

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -6,11 +6,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @SpringBootApplication
 public class MainClass 
 {
-
+private static final Logger logger = LogManager.getLogger(MainClass.class);
     public static void main(String[] args) {
+
+	        logger.debug("Debugging log");
+	            logger.info("Info log");
+	            logger.warn("Hey, This is a warning!");
+	        logger.error("Oops! We have an Error. OK");
+            logger.fatal("Damn! Fatal error. Please fix me.");
+
         ApplicationContext ctx = SpringApplication.run(MainClass.class, args);
 
         System.out.println("Let's inspect the beans provided by Spring Boot:");
